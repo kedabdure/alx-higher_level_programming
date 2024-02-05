8175588212b7914f504380388cc7aca60cc653bb
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-06 01:53:45 +0300

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        return value.isdigit()
-    except AttributeError:
-        return False
+def safe_print_integer(value):try:
+    print("{:d}".format(value))
+    return True
+except (TypeError, ValueError):
+    return False

4d5c31dc78eb161bb86c6402b2feb085a2600024
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-07 22:28:06 +0300

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""defining the class square."""
+
+
+class Square:
+    """representing the square"""
+    
+    def __init__(self, size=0):
+        """defining the methode
+        
+        Args:
+            size (int) represent the size of the square.
+        """
+        self.__size = size
+        
+    def area(self):
+        """Return the current area"""
+        
+        return self.__size * self.__size

55ea7810d5a459e23411b4c1bcd4c86abad8294a
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-07 23:28:57 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -27,5 +27,8 @@ class Square:
     @size.setter
     def size(self, value):
         """modify the original value"""
-
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value

96280f8b537492da8a2d0be228b5105d1accd8f7
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-14 08:58:11 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 calculate the sum of the two number
 """
 
+
 def add_integer(a, b=98):
     """
     Return the sum of two number
@@ -10,15 +11,14 @@ def add_integer(a, b=98):
     Args:
         a (int): first number.
         b (int): second number.
-   
+
     Raises:
         TypeError: if a or b arn't intiger or float number
-        
+
     Return:
         int: the sum of a and b.
     """
-    
-    
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError('a must be an integer')
 
@@ -27,5 +27,5 @@ def add_integer(a, b=98):
 
     a = int(a)
     b = int(b)
-        
+
     return a + b

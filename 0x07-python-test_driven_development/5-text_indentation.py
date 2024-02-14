5acfb112f8b1435a630524de863d100bb0a5a515
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-14 15:24:40 +0300

    Create 5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Print a text with two new line"""
+
+
+def text_indentation(text):
+    """print indented test
+
+    Args:
+        text (str): text content
+    """
+
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    new_text = ''
+    penctuation = '.?:'
+
+    for char in text:
+        new_text += char
+        for penc in penctuation:
+            if penc == char:
+                new_text += '\n\n'
+
+    line = [(line.strip()) for line in (new_text.split('\n\n'))]
+    final_text = '\n\n'.join(line)
+
+    print(final_text)

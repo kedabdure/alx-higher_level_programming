f0ffa47f1d467096892d4e71cb5128d620403f0f
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-19 23:23:58 +0300

    Create 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""BaseGeometry module"""
+
+
+class BaseGeometry:
+    """geometry"""
+
+    def area(self):
+        """Raise an exception massage"""
+
+        raise Exception("area() is not implemented")

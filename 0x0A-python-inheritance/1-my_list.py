22c597437e78ba2b5dcaeb8f328b89170e0668c4
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-19 13:54:54 +0300

    Create 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+class MyList(list):
+    """subclass of list"""
+
+    def print_sorted(self):
+        """print the sorted list"""
+        sorted_list = sorted(self)
+
+        print (sorted_list)

2ef22dc5135ad28ff55dc6570c6957ab7b632c0e
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-20 03:40:06 +0300

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -50,8 +50,10 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """print the width / height """
+        string = "[{}] ".format(self.__class__.__name__)
+        string += "{}/{}".format(self.__width, self.__height)
 
-        return ("[{}] {}/{}".format(self.__class__.__name__, self.__width, self.__height))
+        return string
 
 
 class Square(Rectangle):

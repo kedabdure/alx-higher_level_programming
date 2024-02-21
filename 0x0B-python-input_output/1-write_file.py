eaee2be99975f1300e5dbeec21b60daa9d76be23
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-21 18:14:00 +0300

    Create 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""write file module"""
+
+
+def write_file(filename="", text=""):
+    """write on file
+
+    Args:
+        filename (str): file name to write on
+        text (str): text to write on file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        """function to write on the file"""
+
+        f.write(text)
+  

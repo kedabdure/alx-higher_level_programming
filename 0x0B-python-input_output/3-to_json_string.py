a09350ab7cf1d70cf4589f4196afd2817e7401bc
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-21 21:02:44 +0300

    Create 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import json
+
+"""to_json_string module"""
+
+
+def to_json_string(my_obj):
+    """return json representation of an object"""
+
+    return json.dumps(my_obj)

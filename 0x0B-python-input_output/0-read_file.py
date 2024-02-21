55586adf07a05abfdaf9b393d6e982e03bd07d5a
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-21 17:41:20 +0300

    Create 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""read file module"""
+
+
+def read_file(filename=""):
+    """read file
+
+    Args:
+        filename (str): file name to be read
+    """
+
+with open("my_file_0.txt") as f:
+    f.read()

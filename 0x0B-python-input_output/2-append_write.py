98ed8bb5f33d7beeae64269d98485c3ea9643333
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-21 20:40:25 +0300

    Create 2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""file_append module"""
+
+
+def append_write(filename="", text=""):
+    """append to a file
+
+    Args:
+        filename (str): file name to write on
+        text (str): text to write on file
+    """
+
+    with open(filename, "a", encoding="utf-8") as f:
+        """append text to the end of the file"""
+
+        return (f.write(text))
+  

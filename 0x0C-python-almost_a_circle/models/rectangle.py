901092e486f7e378c9974c4a108979015dce7d92
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-27 01:40:39 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,7 @@ class Rectangle(Base):
         """display"""
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """print area"""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"

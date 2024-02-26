a728260a4a98bafabd6601fb32974fabd90d4cf6
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-26 21:58:06 +0300

    Update test_rectangle.py

--- 0x0C-python-almost_a_circle/ tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/ tests/test_models/test_rectangle.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+import unittest
+from models.rectangle import Rectangle
 
+class TestRectangle(unittest.TestCase):
+    def test_default_value(self):
+        r1 = Rectangle()
+
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)

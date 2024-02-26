ca14361eae5f7ac970609c10ae9d741a7b3448c7
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-26 22:29:03 +0300

    Update test_rectangle.py

--- 0x0C-python-almost_a_circle/ tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/ tests/test_models/test_rectangle.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python3
+"""Unittest for class rectangle"""
+
 import unittest
+from models.base import Base
 from models.rectangle import Rectangle
 
-class TestRectangle(unittest.TestCase):
-    def test_default_value(self):
-        r1 = Rectangle()
 
-        self.assertEqual(r1.x, 0)
-        self.assertEqual(r1.y, 0)
+class TestRectangle(unittest.TestCase):
+    """Test cases for the Rectangle class."""
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_2_0(self):
+        """Checking for id."""
+
+        r0 = Rectangle(1, 2)
+        self.assertEqual(r0.id, 1)
+        r1 = Rectangle(2, 3)
+        self.assertEqual(r1.id, 2)
+        r2 = Rectangle(3, 4)
+        self.assertEqual(r2.id, 3)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
+        r4 = Rectangle(10, 2, 4, 5, 34)
+        self.assertEqual(r4.id, 34)
+        r5 = Rectangle(10, 2, 4, 5, -5)
+        self.assertEqual(r5.id, -5)
+        r6 = Rectangle(10, 2, 4, 5, 9)
+        self.assertEqual(r6.id, 9)

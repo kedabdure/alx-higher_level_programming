5ac6b4e2515abe698d8733ac4166a3ee0c527e93
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-27 00:02:55 +0300

    Update test_rectangle.py

--- 0x0C-python-almost_a_circle/ tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/ tests/test_models/test_rectangle.py
@@ -2,6 +2,8 @@
 """Unittest for class rectangle"""
 
 import unittest
+import io
+import contextlib
 from models.base import Base
 from models.rectangle import Rectangle
 
@@ -11,21 +13,30 @@ class TestRectangle(unittest.TestCase):
 
     def setUp(self):
         Base._Base__nb_objects = 0
+        
+    def tearDown(self):
+        print("tear down")
 
-    def test_2_0(self):
+    def test_rec_id(self):
         """Checking for id."""
-
         r0 = Rectangle(1, 2)
         self.assertEqual(r0.id, 1)
         r1 = Rectangle(2, 3)
         self.assertEqual(r1.id, 2)
         r2 = Rectangle(3, 4)
         self.assertEqual(r2.id, 3)
+        
+    def test_default_value(self):
+        """checking multiple arg""" 
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
         r4 = Rectangle(10, 2, 4, 5, 34)
         self.assertEqual(r4.id, 34)
+        
+    def test_passing_id(self):
+        """checking custom id"""
         r5 = Rectangle(10, 2, 4, 5, -5)
         self.assertEqual(r5.id, -5)
         r6 = Rectangle(10, 2, 4, 5, 9)
         self.assertEqual(r6.id, 9)
+

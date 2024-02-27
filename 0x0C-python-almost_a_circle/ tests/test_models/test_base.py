f075630594854aa3dacd7cd8545dc82dcb48f37d
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-27 14:11:57 +0300

    Update test_base.py

--- 0x0C-python-almost_a_circle/ tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/ tests/test_models/test_base.py
@@ -9,16 +9,20 @@ class TestBase(unittest.TestCase):
     
     def test_default_value(self):
         b1 = Base()
-        b2 = Base()
-        b3 = Base()
         self.assertEqual(b1.id, 1)
+        b2 = Base()
         self.assertEqual(b2.id, 2)
+        b3 = Base()
         self.assertEqual(b3.id, 3)
+        b4 = Base(4)
+        self.assertEqual(b4.id, 4)
+        b5 = Base(-20)
+        self.assertEqual(b5.id, -20)
+        b7 = Base(600)
+        self.assertEqual(b7.id, 600)
 
-    def test_custom_value(self):
-        b1 = Base(4)
-        b2 = Base(6)
-        b3 = Base(20)
-        self.assertEqual(b1.id, 4)
-        self.assertEqual(b2.id, 6)
-        self.assertEqual(b3.id, 20)
+    def test_for_type(self):
+        b8 = Base()
+        self.assertEqual(type(b8), Base)
+        b9 = Base(8)
+        self.assertEqual(type(b9), Base)

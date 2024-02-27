6e2d571683cd80b0e9ae569de5f54b3ad56740bb
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-27 14:55:57 +0300

    Update test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -40,6 +40,19 @@ class TestRectangle(unittest.TestCase):
         r6 = Rectangle(10, 2, 4, 5, 9)
         self.assertEqual(r6.id, 9)
 
+    def test_passing_string(self):
+        """checking string as id"""
+        with self.assertRaises(TypeError):
+            Rectangle("1", 2)
+        with self.assertRaises(TypeError):
+            Rectangle(1, "2")
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, "3")
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, "4")
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, "4")
+
     def test_area(self):
         """check the area"""
         r7 = Rectangle(4, 6)

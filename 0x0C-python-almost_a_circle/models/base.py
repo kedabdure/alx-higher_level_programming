9c20bcd2a0c9542b7e9d10a1d8086f98e1fe81f0
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-28 11:00:30 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,6 +50,6 @@ class Base:
     def from_json_string(json_string):
         """from json to list"""
         if json_string is None or json_string == '':
-            return "[]"
+            return []
         else:
             return json.loads(json_string)

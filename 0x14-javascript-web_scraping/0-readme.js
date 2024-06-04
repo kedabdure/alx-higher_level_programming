095221b0891b2433cf4bd57014d81d5da51471e1
kedabudre <abdurehimk77@gmail.com>
2024-06-04 23:14:06 +0300

    update file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const fs = require("fs")
+const fs = require('fs');
 
-const filename = process.argv[2]
+const filename = process.argv[2];
 
-fs.readFile(filename, "utf-8", (err, data) => {
-    if(err){
-        console.log(err);
-        return;
-    }
-    console.log(data)
-})
+fs.readFile(filename, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});

e5b995f3961d1fb3488d2e7535cc553c4f127c89
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-06-13 00:43:08 +0300

    Update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,4 +1,7 @@
 const $ = window.$;
-$.get('https://swapi.co/api/people/5/?format=json', function (data, textStatus) {
-  $('DIV#character').text(data.name);
+$.get('https://swapi.co/api/films/?format=json', function (data, textStatus) {
+  const res = data.results;
+  for (let i = 0; i < res.length; i++) {
+    $('UL#list_movies').append('<li>' + res[i].title + '</li>');
+  }
 });
